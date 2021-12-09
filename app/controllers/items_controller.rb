289e470e5b11f0598c0c1f19183cbbc6b8897920
EbiTatsu <tatsuya.ebifly888yen@gmail.com>
2021-12-09 16:53:03 +0900

    ユーザー管理機能実装修正2

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,9 +1,9 @@
 class ItemsController < ApplicationController
   # ログインしていないユーザーはログインページに促す
-  before_action :authenticate_user!, except: [:index, :show]
+  # before_action :authenticate_user!, except: [:index, :show]
 
   # 重複処理をまとめる
-  before_action :set_item, only: [:show, :edit, :update, :destroy]
+  # before_action :set_item, only: [:show, :edit, :update, :destroy]
 
   def index
     # @items = Item.includes(:user).order('created_at DESC')
