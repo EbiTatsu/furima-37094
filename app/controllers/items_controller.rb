d132925bcb433b358f0a4e9a51ac697b308eb79f
EbiTatsu <tatsuya.ebifly888yen@gmail.com>
2021-12-17 15:51:56 +0900

    商品出品機能修正4

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -24,21 +24,21 @@ class ItemsController < ApplicationController
 
   def edit
     # ログインしているユーザーと同一であればeditファイルが読み込まれる
-    if @item.user_id == current_user.id && @item.order.nil?
-    else
-      redirect_to root_path
-    end
+    # if @item.user_id == current_user.id && @item.order.nil?
+    # else
+      # redirect_to root_path
+    # end
   end
 
   def update
-    @item.update(item_params)
+    # @item.update(item_params)
     # バリデーションがOKであれば詳細画面へ
-    if @item.valid?
-      redirect_to item_path(item_params)
-    else
+    # if @item.valid?
+      # redirect_to item_path(item_params)
+    # else
       # NGであれば、エラー内容とデータを保持したままeditファイルを読み込み、エラーメッセージを表示させる
-      render 'edit'
-    end
+      # render 'edit'
+    # end
   end
 
   def show
--- app/models/item.rb
+++ app/models/item.rb
@@ -18,7 +18,7 @@ extend ActiveHash::Associations::ActiveRecordExtensions
   has_one_attached :image
 
   with_options presence: true do
-    validates :user_id
+    # validates :user_id
     validates :image
     validates :name
     validates :description
@@ -28,7 +28,7 @@ extend ActiveHash::Associations::ActiveRecordExtensions
     validates :prefecture_id
     validates :shipping_date_id
     # 300円以上かつ9,999,999円以下で、半角数字でないと入力不可
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, only_integer: true
   end
 
   # ジャンルの選択が「--」の時は保存不可
