a13c1c8308ef6bd6e4a4710c92b6a2065169ac52
EbiTatsu <tatsuya.ebifly888yen@gmail.com>
2021-12-17 21:13:18 +0900

    商品一覧表示機能実装1

--- app/models/item.rb
+++ app/models/item.rb
@@ -28,7 +28,7 @@ extend ActiveHash::Associations::ActiveRecordExtensions
     validates :prefecture_id
     validates :shipping_date_id
     # 300円以上かつ9,999,999円以下で、半角数字でないと入力不可
-    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }, only_integer: true
+    validates :price, numericality: { greater_than_or_equal_to: 300, less_than_or_equal_to: 9_999_999 }
   end
 
   # ジャンルの選択が「--」の時は保存不可
